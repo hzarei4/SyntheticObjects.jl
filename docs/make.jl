0b174bef781a58b493283fecdf1df11472096ba9
hzarei4 <hzoshtolagh@gmail.com>
2024-06-12 14:46:39 +0200

    updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,9 @@
 using Documenter, SyntheticObjects
 
-makedocs(sitename="SyntheticObjects.jl"
-        , format="html"
-        , doctest = true
-        , modules = [SyntheticObjects]
+makedocs(sitename="SyntheticObjects.jl",
+        #format="html"
+        doctest = false,
+        modules = [SyntheticObjects]
         , pages = Any[
             "SyntheticObjects.jl" => "index.md",
         ]
@@ -12,13 +12,6 @@ makedocs(sitename="SyntheticObjects.jl"
         )
 
 
-deploydocs(repo = "github.com/hzarei4/SyntheticObjects.jl.git" 
-            , target = "gh-pages"
-            , sitename = "SyntheticObjects.jl"
-            , modules = [SyntheticObjects]
-            , pages = Any[
-                "SyntheticObjects.jl" => "index.md",
-            ]
-            , 
-            # make = Documenter.make_julia_cmd()
+deploydocs(repo = "github.com/hzarei4/SyntheticObjects.jl.git",
+            target = "gh-pages",
             )
